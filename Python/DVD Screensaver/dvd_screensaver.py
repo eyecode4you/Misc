79b98d3e65ae0bf969b8a3e65838dcaf25117fed
EyeCode4You_Host <77638909+eyecode4you@users.noreply.github.com>
2023-12-20 05:32:46 +0000

    Update dvd_screensaver.py

--- Python/DVD Screensaver/dvd_screensaver.py
+++ Python/DVD Screensaver/dvd_screensaver.py
@@ -12,7 +12,7 @@ screen = pygame.display.set_mode((800, 600))
 pygame.display.set_caption("Will it Ever Hit a Corner?")
 
 # dvd icon
-dvd = pygame.image.load("./resources/dvdlogo.png")
+dvd = pygame.image.load("./dvdlogo.png")
 dvd = pygame.transform.scale(dvd, (138, 66))
 dvd = dvd.convert_alpha()
 dvd_rect = dvd.get_rect()
